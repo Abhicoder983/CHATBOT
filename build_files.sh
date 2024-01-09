d21e2a951129ab095ad2d3447fc2cb589e135a85
Abhishek kumar <112263396+Abhicoder983@users.noreply.github.com>
2024-01-09 18:36:02 +0000

    changes

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python5.0 -m pip install -r requirements.txt
-python5.0 manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
