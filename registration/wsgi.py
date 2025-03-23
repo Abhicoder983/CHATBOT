1b92eec7a2217bcd28e27657897cff56238a407c
abhishek kumar <ruhelabhishek@gmail.com>
2025-03-24 01:54:57 +0530

    changes

--- registration/wsgi.py
+++ registration/wsgi.py
@@ -1,14 +1,8 @@
-"""
-WSGI config for registration project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import logging
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'registration.settings')
+logging.basicConfig(level=logging.DEBUG)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+
 application = get_wsgi_application()
