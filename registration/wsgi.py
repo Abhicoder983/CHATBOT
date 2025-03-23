8bd51511ce7dcc3b78d485aeac57d4445e9718d8
abhishek kumar <ruhelabhishek@gmail.com>
2025-03-24 02:46:39 +0530

    changes

--- registration/wsgi.py
+++ registration/wsgi.py
@@ -3,7 +3,7 @@ import logging
 from django.core.wsgi import get_wsgi_application
 
 logging.basicConfig(level=logging.DEBUG)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'registration.settings')
 
 application = get_wsgi_application()
 
